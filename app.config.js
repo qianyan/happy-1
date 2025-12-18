8d7c36fcbd273716f0eaebd0d5f8840ad1c8aab8
Kyle Fang <zhigang1992@gmail.com>
2025-12-18 14:22:09 +0800

    wip

--- app.config.js
+++ app.config.js
@@ -15,7 +15,7 @@ export default {
         name,
         slug: "happy",
         version: "1.5.1",
-        runtimeVersion: "19",
+        runtimeVersion: "20",
         orientation: "default",
         icon: "./sources/assets/images/icon.png",
         scheme: "happy",
